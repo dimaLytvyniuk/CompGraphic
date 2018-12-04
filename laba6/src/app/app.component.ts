a7139ff346e25c9037cc1d648ff4a63a48739b57
dimaLytvyniuk <dima.lytvyniuk@gmail.com>
2018-12-04 23:54:16 +0200

    End laba6

--- laba6/src/app/app.component.ts
+++ laba6/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import * as THREE from 'three';
+import { NullTemplateVisitor } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,10 @@ export class AppComponent {
   renderer = new THREE.WebGLRenderer();
   scene = null;
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-  mesh = null;
+  meshX = null;
+  meshY = null;
+  meshXY = null;
+  meshXYZ = null;
 
   constructor() {
     this.scene = new THREE.Scene();
@@ -21,11 +25,28 @@ export class AppComponent {
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
         this.camera.position.z = 1000;
 
-        const geometry = new THREE.ConeGeometry( 100, 250, 3, 2, true);
+        const geometry = new THREE.CylinderBufferGeometry(150, 70, 150, 3)
+        
+        // ForX
         const material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-        this.mesh = new THREE.Mesh(geometry, material);
+        this.meshX = new THREE.Mesh(geometry, material);
+        this.meshX.position.set( -700, 0, 200 );
+        // ForY
+        this.meshY = new THREE.Mesh(geometry, material);
+        this.meshY.position.set( -250, 0, 200 );
 
-        this.scene.add(this.mesh);
+        // ForXY
+        this.meshXY = new THREE.Mesh(geometry, material);
+        this.meshXY.position.set( 100, 0, 200 );
+
+        // ForXYZ
+        this.meshXYZ = new THREE.Mesh(geometry, material);
+        this.meshXYZ.position.set( 500, 0, 200 );
+
+        this.scene.add(this.meshX);
+        this.scene.add(this.meshY);
+        this.scene.add(this.meshXY);
+        this.scene.add(this.meshXYZ);
   }
 
   ngAfterViewInit() {
@@ -36,8 +57,13 @@ export class AppComponent {
 
   animate() {
     window.requestAnimationFrame(() => this.animate());
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.02;
+    this.meshX.rotation.x += 0.015;
+    this.meshY.rotation.y += 0.01;
+    this.meshXY.rotation.x += 0.015;
+    this.meshXY.rotation.y += 0.01;
+    this.meshXYZ.rotation.x += 0.015;
+    this.meshXYZ.rotation.y += 0.01;
+    this.meshXYZ.rotation.z += 0.02;
     this.renderer.render(this.scene, this.camera);
   }
 }
